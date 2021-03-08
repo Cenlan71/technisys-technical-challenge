a0f8aac327f10cd3d995ec226d9bb10c352be141
Napoleon <napoleonsgf@gmail.com>
2021-03-07 23:29:39 -0300

    Update primeController.js
    Fixes en index

--- src/controllers/primeController.js
+++ src/controllers/primeController.js
@@ -5,15 +5,27 @@ exports.evaluate = function (req, res) {
   let baseNumber = parseInt(req.body.baseNumber);
 
   if (inputNumber !== parseInt(inputNumber, 10)) { //Si el input no es un número, no permite seguir, y muestra un mensaje al usuario.
-    // console.log(`Solo puedes introducir números, que sean mayores o iguales a ${baseNumber}.`);
-    // console.log(`----------------------------------------------------------------------------------------------------`);
-    // return false;
+    let answer = '<h1>Solo puedes introducir números</h1>';
+    answer += '<hr>';
+    answer += '<p>Si lo deseas, puedes evaluar otro número.</p>';
+    answer += '<form method="POST" action="/api/evaluate">';
+    answer += '<input type="text" name="inputNumber"/>';
+    answer += '<input type="hidden" name="baseNumber" value="'+baseNumber+'"/>';
+    answer += '<button type="submit"/>Enviar</button>';
+    answer += '</form>';
+    res.send(answer);
   }
 
   if (inputNumber < baseNumber || inputNumber <= 1) { //Si el número introducido por input es menor al número base, el algoritmo no permite seguir, y le advierte de esto al usuario.
-    // console.log(`Número inválido. Introduce un número mayor o igual ${baseNumber}.`);
-    // console.log(`----------------------------------------------------------------------------------------------------`);
-    // return false;
+    let answer = '<h1>Debes introducir un número mayor o igual a '+baseNumber+'</h1>';
+    answer += '<hr>';
+    answer += '<p>Si lo deseas, puedes evaluar otro número.</p>';
+    answer += '<form method="POST" action="/api/evaluate">';
+    answer += '<input type="text" name="inputNumber"/>';
+    answer += '<input type="hidden" name="baseNumber" value="'+baseNumber+'"/>';
+    answer += '<button type="submit"/>Enviar</button>';
+    answer += '</form>';
+    res.send(answer);
   }
 
   let printableNumbers = [];
